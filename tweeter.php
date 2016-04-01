d112b582215e06cf6f26eb8983c81eb0e74811c7
Joe Ferguson <joe@joeferguson.me>
2016-04-01 13:59:52 -0500

    gracefully catch if the user is missing ENV variables

--- tweeter.php
+++ tweeter.php
@@ -6,10 +6,24 @@ require __DIR__.'/vendor/autoload.php';
 use Memtech\Console\Command\TweeterCommand;
 use Symfony\Component\Console\Application;
 
-// Set up the environment variables
-$dotenv = new Dotenv\Dotenv(__DIR__);
-$dotenv->load();
-$dotenv->required(['TWITTER_KEY','TWITTER_SECRET','TWITTER_TOKEN','TWITTER_TOKEN_SECRET','MEETUP_KEY','GOOGLE_SHORTEN_KEY']);
+try
+{
+    // Set up the environment variables
+    $dotenv = new Dotenv\Dotenv(__DIR__);
+    $dotenv->load();
+    $dotenv->required([
+        'TWITTER_KEY',
+        'TWITTER_SECRET',
+        'TWITTER_TOKEN',
+        'TWITTER_TOKEN_SECRET',
+        'MEETUP_KEY',
+        'GOOGLE_SHORTEN_KEY'
+    ]);
+}
+catch (\Dotenv\Exception\ValidationException $e)
+{
+    exit($e->getMessage());
+}
 
 $application = new Application();
 $application->add(new TweeterCommand());
